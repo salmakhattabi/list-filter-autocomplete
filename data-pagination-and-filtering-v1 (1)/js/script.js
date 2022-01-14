07748092d33707ee4f2a7988be68d36253b16c92
monsif <monsif.khattabi@baanx.com>
2022-01-14 23:20:57 +0100

    - Add filter and autoComplete

--- data-pagination-and-filtering-v1 (1)/js/script.js
+++ data-pagination-and-filtering-v1 (1)/js/script.js
@@ -2,12 +2,46 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
-function createSearchField() {
-  let header = document.querySelector("header");
-
+function filterResults(students) {
+  // Get search query value
+  const search = document.getElementById('search').value.toLowerCase();
+  // Filter list by search query value
+  return students.filter((student) => checkIfQueryInName(student,search));
 }
-createSearchField();
+
+function checkIfQueryInName(student,search) {
+  return formatName(student.name).toLowerCase().indexOf(search) !== -1
+              || student.email.toLowerCase().indexOf(search) !== -1;
+}
+
+function showThis(name) {
+  document.getElementById('search').value = name;
+  showPage(data,1);
+}
+
+function addAutoComplete(list) {
+  const autocompleteContainer = document.getElementById('results');
+  autocompleteContainer.innerHTML = '';
+  list.forEach((el) => {
+
+    autocompleteContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="autocomplete-item" onClick="showThis('${formatName(el.name)}')">
+        <img src="${el.picture.thumbnail}" alt="${formatName(el.name)}" />
+        <div class="info-container">
+          <p>${formatName(el.name)}</p>
+          <p>${el.email}</p>
+        </div>
+      </div>
+    `
+    );
+  });
+}
+
+function formatName(name) {
+  return `${name.title} ${name.first} ${name.last}`;
+}
 
 /*
 For assistance:
@@ -15,6 +49,7 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 let searchBar = document.querySelector('.header');
+let currentPage = 1;
 
 
 
@@ -24,8 +59,49 @@ This function will create and insert/append the elements needed to display a "pa
 */
 // create two variables which will represent the index for the first and last student on the page
 
-function showPage(list, page) {
+function createSearchField() {
+  searchBar.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="search-bar">
+      <label for="search" class="student-search">
+        <span>Search by name</span>
+        <input id="search" placeholder="Search by name...">
+        <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+      </label>
+      <div id="results" class="autocomplete">
+      </div>
+    </div>
+  `
+  );
+  const search = document.getElementById("search");
+  search.addEventListener("input", _ => {
+    filteredResults = showPage(data,currentPage).slice(0,5);
+    const search = document.getElementById("search");
+    if(search.value === '' || filteredResults.length === 0){
+      document.getElementById('results').classList.remove('active');
+    }
+    else if(search.value !== '' && !document.getElementById('results').classList.contains('active'))
+    {
+      document.getElementById('results').classList.add('active');
+    }
+    // Add autocomplete
+    addAutoComplete(filteredResults);
+  });
+  search.addEventListener("blur", _ => {
+    setTimeout(_=>{
+      document.getElementById('results').classList.remove('active');
+    },400)
+  });
+ 
+}
+createSearchField();
 
+function showPage(list, page) {
+  // Filter results
+  const filteredLists = filterResults(list);
+  // Add Pagination
+  addPagination(filteredLists);
   const itemsPerpage = 9;
   const startIndex = (page*itemsPerpage)-itemsPerpage;
   const endIndex= page * itemsPerpage ;
@@ -35,18 +111,18 @@ function showPage(list, page) {
   // set the innerHTML property of the variable you just created to an empty string
   ul.innerHTML="";
  // loop over the length of the `list` parameter
-  for (let i=0; i <list.length ;i++){
-// inside the loop create a conditional to display the proper students
+ // inside the loop create a conditional to display the proper students
+  for (let i=0; i <filteredLists.length ;i++){
     if(i >= startIndex && i < endIndex){
       // create the elements needed to display the student information
       let studentItem = `<li class="student-item cf">
       <div class="student-details">
-      <img class="avatar" src="${data[i].picture.large}">
-      <h3>${data[i].name.first} ${data[i].name.last}</h3>
-      <span class="email">${data[i].email}</span>
+      <img class="avatar" src="${filteredLists[i].picture.large}">
+      <h3>${filteredLists[i].name.first} ${filteredLists[i].name.last}</h3>
+      <span class="email">${filteredLists[i].email}</span>
       </div>
       <div class="joined-details">
-      <span class="date">${data[i].registered.date}</span>
+      <span class="date">${filteredLists[i].registered.date}</span>
       </div>`
        // insert the above elements
       ul.insertAdjacentHTML('beforeend', studentItem);
@@ -54,8 +130,9 @@ function showPage(list, page) {
 
   };
 
+  return filteredLists;
 };
- showPage(data, 1);
+ showPage(data, currentPage);
 
 
 
@@ -67,6 +144,7 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 function addPagination(list) {
+
 // create a variable to calculate the number of pages needed
    let numberOfItems = Math.ceil(list.length / 9);
 // select the element with a class of `link-list` and assign it to a variable
@@ -98,8 +176,8 @@ function addPagination(list) {
         // remove the "active" class from the previous button
          document.querySelector('.active').className = '';
          event.target.className = 'active';
+         currentPage = event.target.textContent;
          showPage(list, event.target.textContent);
       };
    });
 };
-addPagination(data);
